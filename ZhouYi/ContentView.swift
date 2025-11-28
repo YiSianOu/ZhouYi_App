d4e3b096c1d1272cb671e611615a18d7ebee09f5
YiSianOu <pme123@gapp.nthu.edu.tw>
2025-11-29 01:46:59 +0800

    Initial Commit

---
+++ ZhouYi/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ZhouYi
+//
+//  Created by 歐翊嫻 on 2025/11/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ZhouYi/ZhouYiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ZhouYiApp.swift
+//  ZhouYi
+//
+//  Created by 歐翊嫻 on 2025/11/29.
+//
+
+import SwiftUI
+
+@main
+struct ZhouYiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
